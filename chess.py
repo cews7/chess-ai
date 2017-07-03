ad96fc95f9ab49b7a05eaa030f7806b3952db579
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-07-02 20:44:41 -0600

    add policies for rooks, pawns; add general move policies

--- chess.py
+++ chess.py
@@ -31,12 +31,23 @@ class chessBoard:
                     self.board[x][y]=piece('king','black',(x,y))
                 else:
                     self.board[x][y]=piece(' ','green',(x,y))
+
     def printBoard(self):
         for x in range(8):
             row=''
             for y in range(8):
-                row=row +'| '+self.board[x][y].name[0]+ ' |'
+                row=row +'| '+ self.board[x][y].name[0] + ' |'
             print(row)
+
+    def movePiece(self,p,position):
+        x,y=position[0],position[1]
+        piece_x,piece_y=p.position[0],p.position[1]
+        if p.canMove(position,self.board):
+            self.board[x][y]=p
+            self.board[piece_x][piece_y]=piece(' ','green',(piece_x,piece_y))
+            p.position=(x,y)
+        else:
+            return False
 
 
 class piece:
@@ -45,4 +56,65 @@ class piece:
         self.color=color
         self.position=position
 
-    def canMove(position):
+    def getOtherColor(self):
+        if self.color=='white':
+            return 'black'
+        else:
+            return 'white'
+    def canMove(self,position,board):
+        x,y=position[0], position[1]
+        piece_x,piece_y=self.position[0],self.position[1]
+        if self.name=='pawn':
+            if self.color=='white':
+                if board[x][y].name==' ':
+                    if self.position[0]==1:
+                        if y==self.position[1] and (x==3 or x==2):
+                            if board[2][y].name==' ':
+                                return True
+                    else:
+                        if y==self.position[1] and (x==1+self.position[0]):
+                            return True
+                else:
+                    if self.position[0]+1==x and (self.position[1]==y+1 or self.position[1]==y-1) and board[x][y].color=='black':
+                        return board[x][y]
+            if self.color=='black':
+                if board[x][y].name==' ':
+                    if self.position[0]==6:
+                        if y==self.position[1] and (x==4 or x==5):
+                            if board[5][y].name==' ':
+                                return True
+                    else:
+                        if y==self.position[1] and (x==-1+self.position[0]):
+                            return True
+                else:
+                    if self.position[0]-1==x and (self.position[1]==y+1 or piece.position[1]==y-1) and board[x][y].color=='white':
+                        return board[x][y]
+            return False
+        if self.name=='rook':
+            if piece_x==x:
+                for i in range(min(piece_y,y),max(piece_y,y))[1:]:
+                    if board[x][i].name!=' ':
+                        return False
+                if board[x][y].color==self.getOtherColor():
+                    return board[x][y]
+                elif board[x][y].color==self.color:
+                    return False
+                else:
+                    return True
+
+            if piece_y==y:
+                for i in range(min(piece_x,x),max(piece_x,x))[1:]:
+                    if board[i][y].name!=' ':
+                        return False
+                if board[x][y].color==self.getOtherColor():
+                    return board[x][y]
+                elif board[x][y].color==self.color:
+                    return False
+                else:
+                    return True
+
+
+
+chess=chessBoard('tim')
+rook=chess.board[0][0]
+pawn = chess.board[1][3]
